ca913e9c595317c48a488e2157b2f6017e42b10a
tinyz <zou90512@126.com>
2016-09-15 23:18:38 +0800

    Add : Hello World

--- lessons/my_http.py
+++ lessons/my_http.py
@@ -9,15 +9,34 @@ params = urllib.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'
 print params
 
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-try:
-    httpClient = httplib.HTTPConnection('www.baidu.com', 80)
 
-    httpClient.request('GET', '')
+
+def status_200(resp):
+    var = resp.read()
+    print(var)
+    return
+
+
+def status_default(resp):
+    print('[WARN] Status:', resp.status, ', Reason:', resp.reason)
+    return
+
+try:
+    httpClient = httplib.HTTPConnection('tinyzzh.github.io', 80)
+
+    httpClient.request('GET', '/xxx/11')
     response = httpClient.getresponse()
-    if response.status == 200:
-        data = response.read()
-        print data
-    else:
-        print response.reason
+
+    switch = {
+        200: status_200,
+    }
+    switch.get(response.status, status_default)(response)
+
+    # if response.status == 200:
+    #     data = response.read()
+    #     print (data)
+    # else:
+    #     print ("[WARN] Status:", response.status, ", Reason:", response.reason)
 except Exception, e:
     print e
+
