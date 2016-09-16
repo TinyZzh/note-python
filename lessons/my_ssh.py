f02490bc4eca13dc4ddd82d5cccb80fd156bd891
TinyZ <tinyzzh815@gmail.com>
2016-09-16 17:17:27 +0800

    Add : ssh - 范例

--- lessons/my_ssh.py
+++ lessons/my_ssh.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+
 import paramiko
+import sys
+
+# sys.argv
+
+host = '192.168.230.129'
+port = 22
 
 client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+client.connect(host, port, 'root', 'centos')
+# stdin, stdout, stderr = client.exec_command('date -R')
+stdin, stdout, stderr = client.exec_command('iostat')
 
+while not stdout.channel.exit_status_ready():
+
+    cmd_result = stdout.read(), stderr.read()
+    for line in cmd_result:
+        print line,
+    pass
+
+client.close()
+
+print "endl"
 
 
