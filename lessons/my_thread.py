166570ea2d211908422907f7c2b4c68498ee0f6d
TinyZ <tinyzzh815@gmail.com>
2016-09-16 17:17:11 +0800

    Add : 多线程 - 范例

--- lessons/my_thread.py
+++ lessons/my_thread.py
@@ -9,20 +9,21 @@ import threading
 def show_world(name):
     cur_thread = threading.currentThread()
 
-    print "Thread : " + cur_thread.name + "print : " + name
+    print "Thread : " + cur_thread.name + " : " + name
     return
 
 
 try:
-    thd1 = thread.start_new_thread(show_world, ("xx",))
-    thd2 = thread.start_new_thread(show_world, ("yy",))
+    thd1 = threading.Thread(target=show_world, name="Thread-xx", args=("xx",))
+    thd2 = threading.Thread(target=show_world, name="Thread-yy", args=("yy",))
 
+    thd1.start()
+    thd2.start()
 
+    thd1.join()
+    thd2.join()
 
-except:
-    print "Error: unable to start thread"
+except Exception, e:
+    print "Error: unable to start thread : " + e
 
 print "endl"
-
-while 1:
-    pass
