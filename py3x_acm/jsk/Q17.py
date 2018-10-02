34fdf008483d10643323d480fdddd986d23fd00e
zzh <zzh@wooduan.com>
2018-10-02 18:06:24 +0800

    Q17 and Q18

---
+++ py3x_acm/jsk/Q17.py
@@ -0,0 +1,9 @@
+
+# Q17: https://nanti.jisuanke.com/t/17
+
+if __name__ == "__main__":
+    try:
+        while True:
+            print(int(int(input()) ** 0.5))
+    except EOFError:
+        pass
